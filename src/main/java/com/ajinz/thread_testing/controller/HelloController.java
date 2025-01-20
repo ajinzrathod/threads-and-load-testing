239bd9fb70fabe6f57d8cb9db9759d253c642578
ajinzrathod <rajinkya26@gmail.com>
2025-01-20 13:02:41 +0530

    01-Print ip

--- src/main/java/com/ajinz/thread_testing/controller/HelloController.java
+++ src/main/java/com/ajinz/thread_testing/controller/HelloController.java
@@ -1,13 +1,13 @@
 package com.ajinz.thread_testing.controller;
 
 import com.ajinz.thread_testing.service.HelloService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Random;
 
 @RestController
@@ -15,18 +15,15 @@ public class HelloController {
 
   @Autowired HelloService helloService = new HelloService();
 
-  @GetMapping({"ping", "ping/", "/"})
-  public String health() {
+  @GetMapping({"ping", "ping/", ""})
+  public String health()  {
     return "Pong";
   }
 
   @GetMapping({"hello", "hello/"})
-  public String hello(
-      HttpServletRequest request,
-      @RequestHeader(value = "X-Forwarded-For", required = false) String xForwardedFor)
-      throws InterruptedException {
+  public String hello() throws InterruptedException, UnknownHostException {
+    InetAddress localHost = InetAddress.getByName(InetAddress.getLocalHost().getHostAddress());
     Thread.sleep(3000);
-    String clientIp = xForwardedFor != null ? xForwardedFor : request.getRemoteAddr();
-    return "<h1>Hello " + clientIp + "</h1>";
+    return "<h1>Hello " + localHost + "</h1>";
   }
 }
